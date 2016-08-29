0aecb88e67f47537973b7ad93abb9694519bd9d1
Harvey Sanders <sanders.harvey@gmail.com>
2016-08-29 14:07:27 -0500

    (fix) update example task

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -2,8 +2,17 @@ const express = require('express');
 
 const router = express.Router(); // eslint-disable-line new-cap
 
+const exampleTask = {
+  name: 'cut my lawn',
+  location: {
+    address: '748 Camp',
+    city: 'New Orleans',
+    state: 'LA',
+  },
+};
+
 router.get('/', (req, res) => {
-  res.json({ taks: ['cut my lawn'] });
+  res.json([exampleTask]);
 });
 
 module.exports = router;

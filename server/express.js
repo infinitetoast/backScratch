33ff33ed7d92f0dfbab03bf8272fca000adaaa07
Harvey Sanders <sanders.harvey@gmail.com>
2016-08-29 09:09:38 -0500

    (feat) add initial neo4j connection

---
+++ server/express.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const routes = require('../server/routes');
+require('../db');
+
+const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(logger('dev'));
+}
+
+// parse body params and attache them to req.body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// mount all routes on /api/v1 path
+app.use('/api/v1', routes);
+
+module.exports = app;

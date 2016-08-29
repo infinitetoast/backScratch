653d73562f6285cb2754bf5e7d3305fd0f513f92
Harvey Sanders <sanders.harvey@gmail.com>
2016-08-29 13:32:39 -0500

    (feat) rewrite initial routes

---
+++ server/routes/tasks.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router(); // eslint-disable-line new-cap
+
+router.get('/', (req, res) => {
+  res.json({taks: ['cut my lawn']});
+});
+
+module.exports = router;
---
+++ server/routes/users.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router(); // eslint-disable-line new-cap
+
+router.get('/', (req, res) => {
+  res.json({users: ['some guy', 'another guy']});
+});
+
+module.exports = router;

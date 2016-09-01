bcd419a759d8128457405c248c3d03207a5f2662
Harvey Sanders <sanders.harvey@gmail.com>
2016-09-01 12:14:07 -0500

    (chore) remove extra code

--- models/task.js
+++ models/task.js
@@ -7,29 +7,7 @@ module.exports = {
     });
   },
   getAllTasks: () => {
-    // session
-    //   .run(`
-    //     CREATE (a:Person 
-    //       {
-    //         name:'Arthur',
-    //         title:'King'
-    //       }
-    //     )
-    //   `)
-    //   .then(() => session.run(`
-    //     MATCH (a:Person)
-    //     WHERE a.name = 'Arthur'
-    //     RETURN a.name AS name, a.title AS title`
-    //   ))
-    //   .then(result => {
-    //     console.log('neo4j result');
-    //     console.log(`${result.records[0].get('title')} ${result.records[0].get('name')}`);
-    //     res.send('neo4j');
-    //     session.close();
-    //     driver.close();
-    //   }).catch(err => {
-    //     console.error('error with neo4j', err);
-    //   });
+
   },
   getTaskById: (taskId) => {
      // taskId is string
--- models/user.js
+++ models/user.js
@@ -5,49 +5,33 @@ module.exports = {
 
   },
   getAllUsers: () => {
-    session
-      .run(`
-        CREATE (a:Person 
-          {
-            name:'Arthur',
-            title:'King'
-          }
-        )
-      `)
-      .then(() => session.run(`
-        MATCH (a:Person)
-        WHERE a.name = 'Arthur'
-        RETURN a.name AS name, a.title AS title`
-      ))
-      .then(result => {
-        console.log('neo4j result');
-        console.log(`${result.records[0].get('title')} ${result.records[0].get('name')}`);
-        session.close();
-        driver.close();
-      }).catch(err => {
-        console.error('error with neo4j', err);
-      });
+    // session
+    //   .run(`
+    //     CREATE (a:Person 
+    //       {
+    //         name:'Arthur',
+    //         title:'King'
+    //       }
+    //     )
+    //   `)
+    //   .then(() => session.run(`
+    //     MATCH (a:Person)
+    //     WHERE a.name = 'Arthur'
+    //     RETURN a.name AS name, a.title AS title`
+    //   ))
+    //   .then(result => {
+    //     console.log('neo4j result');
+    //     console.log(`${result.records[0].get('title')} ${result.records[0].get('name')}`);
+    //     session.close();
+    //     driver.close();
+    //   }).catch(err => {
+    //     console.error('error with neo4j', err);
+    //   });
   },
   getUserById: (userId) => {
-     /*
-      req.body = {
-        userId: 242,
-        propToUpdate: newVal
-      }
-    */
-    let testMsg = 'user updated';
-    console.log(testMsg);
-    res.send(testMsg);
+    
   },
   updateUser: (userId, newPropsObj) => {
-    /*
-      req.body = {
-        userId: 242,
-        propToUpdate: newVal
-      }
-    */
-    let testMsg = 'user updated';
-    console.log(testMsg);
-    res.send(testMsg);
+    
   },
 };

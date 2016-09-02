91e85e78346920253232d4157917c0f8154e93d9
Harvey Sanders <sanders.harvey@gmail.com>
2016-09-02 12:08:32 -0500

    (chore) remove dummy data

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -3,15 +3,6 @@ const Task = require('../../models/task');
 
 const router = express.Router(); // eslint-disable-line new-cap
 
-const exampleTask = {
-  name: 'cut my lawn',
-  location: {
-    address: '748 Camp',
-    city: 'New Orleans',
-    state: 'LA',
-  },
-};
-
 router.get('/', (req, res, next) => {
   Task.getAllTasks()
     .then(tasks => {

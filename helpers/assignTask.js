3af41102752772453ee5539b4c2847b1783aca90
1kspears23 <1kspears23@gmail.com>
2016-09-09 18:45:55 -0500

    (fix) assign impout where backwards

--- helpers/assignTask.js
+++ helpers/assignTask.js
@@ -1,13 +1,13 @@
 const task = require('../models/task');
 
-task.assignTasks(4, 15);
-task.assignTasks(5, 16);
-task.assignTasks(6, 17);
-task.assignTasks(7, 18);
-task.assignTasks(8, 19);
-task.assignTasks(9, 21);
-task.assignTasks(10, 22);
-task.assignTasks(11, 23);
-task.assignTasks(12, 24);
-task.assignTasks(13, 25);
-task.assignTasks(14, 17);
+task.assignTasks(15, 4);
+task.assignTasks(16, 5);
+task.assignTasks(17, 6);
+task.assignTasks(18, 7);
+task.assignTasks(19, 8);
+task.assignTasks(21, 9);
+task.assignTasks(22, 10);
+task.assignTasks(23, 11);
+task.assignTasks(24, 12);
+task.assignTasks(25, 13);
+task.assignTasks(17, 14);

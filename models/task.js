dd88a99b7e1004e6549d4eaf3e7b30205b55ff77
1kspears23 <1kspears23@gmail.com>
2016-09-09 10:26:59 -0500

    (chore) updating assign task

--- models/task.js
+++ models/task.js
@@ -62,6 +62,22 @@ module.exports = {
       });
     })
   ),
+  assignTasks: (taskId, userId) => (
+    new Promise((resolve, reject) => {
+      db.cypher({
+        query: '',
+      }, (err, results) => {
+        if (err) reject(err);
+        if (!results.length) {
+          console.log('No task found.');
+          resolve({ message: 'No tasks found on the server' });
+        } else {
+          console.log(`Sending ${results.length} tasks`);
+          resolve(results);
+        }
+      });
+    })
+  ),
   getTaskById: (taskId) => (
     // Promise template
     new Promise((resolve, reject) => {

290f36e94eb1d45f9f861dd894138c32d4033077
NealTaylor715 <neal.taylorjr@gmail.com>
2016-09-14 12:36:41 -0500

    (feat) email successfully sent to assignee and requestor with corresponding contact info

--- server/email/email.js
+++ server/email/email.js
@@ -1,4 +1,4 @@
-const dbhelpers = require('../../models/user');
+const dbHelpers = require('../../models/user');
 const nodemailer = require('nodemailer');
 
 let transporter = nodemailer.createTransport({
@@ -17,40 +17,54 @@ let mailOptions = {
 };
 
 const sendEmail = (taskId, assigneeId) => {
-  // requestor's email
-  let requestorOptions = mailOptions;
-  dbhelpers.getUserByTaskId(taskId).then((res) => {
-    const email = res[0].user.properties.email;
-    console.log('taskId: ', taskId);
-    console.log('requestorEmail: ', email);
-    return Object.assign({}, requestorOptions, {to: email, text: 'Hello from backScratch! Someone has accepted your task. Please open the backScratch app for more information.'});
-  }).then((requestorOptions) => {
-    transporter.sendMail(requestorOptions, (error, info) => {
-      if (error) {
-        console.log(error);
+  const getEmailOptions = (taskId, assigneeId) => {
+    let requestorPromise = dbHelpers.getUserByTaskId(taskId);
+    let assigneePromise = dbHelpers.getUserById(assigneeId);
+    return Promise.all([requestorPromise, assigneePromise])
+      .then(results => results.map(result => result[0].user.properties))
+      .then(users => {
+        return {
+          requestor: users[0],
+          assignee: users[1]
+        };
+      })
+      .then(users => {
+        return {
+          requestor: Object.assign(
+            {},
+            mailOptions,
+            {
+              to: users.requestor.email,
+              text: `Hello from backScratch! ${users.assignee.username} has accepted your task. You can contact them at ${users.assignee.email} Please open the backScratch app for more information.`
+            } 
+          ),
+          assignee: Object.assign(
+            {},
+            mailOptions,
+            {
+              to: users.assignee.email,
+              text: `Hello from backScratch! You've accepted ${users.requestor.username} task! You can contact them at ${users.requestor.email} Please open the backScratch app for more information.`
+            } 
+          )
+        };
+      });
+  };
+  getEmailOptions(taskId, assigneeId)
+    .then(options => {
+      for (option in options) {
+        transporter.sendMail(options[option], (error, info) => {
+          if (error) {
+            console.log(error);
+          }
+          else {
+            console.log('Message sent: ' + info.response);
+          };
+        });
       }
-      else {
-        console.log('Message sent: ' + info.response);
-      };
-    });
-  })
-  // assignee's email
-  let assigneeOptions = mailOptions;
-  dbhelpers.getUserById(assigneeId).then((res) => {
-    const email = res[0].user.properties.email;
-    console.log('assigneeId: ', assigneeId);
-    console.log('assigneeEmail: ', email);
-    return Object.assign({}, assigneeOptions, {to: email, text: 'Hello from backScratch! You\'ve accepted Someone\'s task! Please open the backScratch app for more information.'});
-  }).then((assigneeOptions) => {
-    transporter.sendMail(assigneeOptions, (error, info) => {
-      if (error) {
-        console.log(error);
-      }
-      else {
-        console.log('Message sent: ' + info.response);
-      };
-    });
-  })
-};
+    })
+    .catch(err => {
+      console.error(err);
+    })
+}
 
 module.exports = sendEmail;
